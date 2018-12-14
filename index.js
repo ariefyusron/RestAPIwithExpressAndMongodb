b0dd9f9a354b836df52ea7e900f8345d422e2180
Arief Yusron <ariefyusron98@gmail.com>
2018-12-14 14:29:08 +0700

    header access of

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 app.use('/api',routers);
 //error middleware
 app.use((err,req,res,next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.status(422).send({
     err: err.message
   });
